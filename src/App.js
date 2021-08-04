91d2b150a3cac17d27c313fec7620568afc4d7e6
Mintra Domundee <mintra.mynt@gmail.com>
2021-08-04 12:32:23 +0200

    task components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
+
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctor Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at school",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Grocery Shopping",
+      day: "Feb 5th at 2:30pm",
+      reminder: false,
+    },
+  ]);
   const name = "Brad";
 
   return (
     <div className="container">
       <Header title="Task Tracker" />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import PropTypes from "prop-types";
-
+import Button from "./Button";
+const onClick = (e) => {
+  console.log(e);
+};
 const Header = ({ title }) => {
   return (
-    <header>
+    <header className="header">
       <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
     </header>
   );
 };
